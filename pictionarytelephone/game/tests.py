b85de29f8b94f38cbc96eaad0bdf8ac91abd3aa2
Cayenne <cayennes@gmail.com>
2013-08-14 18:58:23 -0500

    First test

--- pictionarytelephone/game/tests.py
+++ pictionarytelephone/game/tests.py
@@ -2,15 +2,17 @@
 This file demonstrates writing tests using the unittest module. These will pass
 when you run "manage.py test".
 
-Replace this with more appropriate tests for your application.
 """
 
-from django.test import TestCase
+from django.test import TestCase, Client
 
 
 class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    def setUp(self):
+        self.client = Client()
+
+
+    def test_has_canvas(self):
+        res = self.client.get('/')
+        self.assertIn('canvas', res.content)
+
