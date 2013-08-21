ad3109bbe5993bde39e9a4d7a7422df7a0926106
Margaret Scott <eudaimonious@gmail.com>
2013-08-21 19:55:24 -0400

    Style buttons

--- pictionarytelephone/game/views.py
+++ pictionarytelephone/game/views.py
@@ -3,11 +3,14 @@ from django.http import HttpResponse
 
 def play(request):
 	    html = """
+	    <head>
 	    	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 	    	<script src="/static/sketch.js"></script>
+	    	<link rel="stylesheet" type="text/css" href="/static/style.css">
+	    </head>
 	    	<div class="tools">
 			  <a href="#tools_sketch" data-tool="marker">Marker</a>
-			  <a href="#tools_sketch" data-tool="eraser">Erase Everything</a>
+			  <a href="#tools_sketch" data-tool="eraser">Eraser</a>
 			</div>
 			<canvas id="tools_sketch" width="800" height="300"></canvas>
 			<script type="text/javascript">
