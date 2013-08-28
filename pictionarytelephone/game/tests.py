9cf25b9ca0d39a4a161752dcbc2a91ef5eefc61d
Margaret Scott <eudaimonious@gmail.com>
2013-08-28 19:48:21 -0400

    add tests for Drawing and Caption previous functions

--- pictionarytelephone/game/tests.py
+++ pictionarytelephone/game/tests.py
@@ -5,14 +5,25 @@ when you run "manage.py test".
 """
 
 from django.test import TestCase, Client
+from game.models import Drawing, Caption
 
-
-class SimpleTest(TestCase):
+class WebTest(TestCase):
     def setUp(self):
         self.client = Client()
-
 
     def test_has_canvas(self):
         res = self.client.get('/')
         self.assertIn('canvas', res.content)
 
+
+class ModelTest(TestCase):
+	def setUp(self):
+		self.drawing = Drawing()
+		self.caption = Caption(previous=self.drawing)
+		self.drawing.previous = self.caption
+
+	def test_previous_drawing(self):
+		self.assertEquals(self.drawing.previous, self.caption)
+
+	def test_previous_caption(self):
+		self.assertEquals(self.caption.previous, self.drawing)
