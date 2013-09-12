3064dcf94443c8b490cec4b391473a73bd7c5d23
Margaret Scott <eudaimonious@gmail.com>
2013-09-11 20:47:11 -0400

    Submit drawing to the server

--- pictionarytelephone/game/models.py
+++ pictionarytelephone/game/models.py
@@ -1,22 +1,8 @@
-import base64
-
 from django.db import models
 
 class Drawing(models.Model):
-
-    _image = models.TextField(
-            db_column='image',
-            blank=True)
-
-    @property
-    def image(self):
-        return base64.decodestring(self._image)
-
-    @image.setter
-    def image(self, image):
-        self._image = base64.encodestring(image)
-
-    previous = models.ForeignKey("Caption")
+    image = models.TextField(blank=True)
+    previous = models.ForeignKey("Caption", null=True)
 
 class Caption(models.Model):
 	previous = models.ForeignKey("Drawing")
