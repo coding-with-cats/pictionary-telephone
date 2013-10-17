594787fddd3439f8e026795a3442052ae7c19aec
Margaret Scott <eudaimonious@gmail.com>
2013-10-16 20:15:33 -0400

    Create user model and attempt to store id in a session

--- pictionarytelephone/game/models.py
+++ pictionarytelephone/game/models.py
@@ -3,7 +3,12 @@ from django.db import models
 class Drawing(models.Model):
     image = models.TextField(blank=True)
     previous = models.ForeignKey("Caption")
+    user = models.ForeignKey("User")
 
 class Caption(models.Model):
-	previous = models.ForeignKey("Drawing", null=True)
-	content = models.CharField(max_length='500')
+    previous = models.ForeignKey("Drawing", null=True)
+    content = models.CharField(max_length='500')
+    user = models.ForeignKey("User")
+
+class User(models.Model):
+    pass
--- pictionarytelephone/game/views.py
+++ pictionarytelephone/game/views.py
@@ -1,18 +1,28 @@
-# Create your views here.
-from django.http import HttpResponse
+# So we got side-tracked (we still need to fill in that null previous) 
+# and are implementing the tracking of users via cookies.
+# Trying to call response.set_cookie. Importing response from django.http
+# Seemed to work but there's no method, set_cookie for 'module'
+from django.http import HttpResponse, response
 from django.shortcuts import redirect, render
-from game.models import Drawing, Caption
+from game.models import Drawing, Caption, User
 
 def play(request):
-
+    obj = None
     if request.POST:
+        if "user_id" in request.COOKIES:
+            user_id = request.COOKIES("user_id")
+            user = User.objects.get(id=user_id)
+        else:
+            user = User()
+            user.save()
+            response.set_cookie("user_id", user.id)
         for field_name, Model, attr in [("image-data", Drawing, 'image'),
                                         ("caption", Caption, 'content')]:
             if field_name in request.POST:
                 obj = Model()
                 setattr(obj, attr, request.POST[field_name])
                 print request.POST[field_name]
-                obj.save()
-
-
-    return render(request, 'index.html', {'drawing': False})
+        obj.user = user
+        obj.save()
+    drawing_val = obj.__class__ == Caption
+    return render(request, 'index.html', {'drawing': drawing_val})
